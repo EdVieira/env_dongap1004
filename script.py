282c7fc85c5b4abf8a0ffbb88c16b536753dc9bf
edd7 <edd7@bitbucket.org>
2018-10-04 02:11:05 -0400

    prints

--- script.py
+++ script.py
@@ -1,23 +1,27 @@
 import os
 
-# Instalar Nginx
+print("Instalando Nginx...")
 os.system("sudo apt-get update")
 os.system("sudo apt-get install -y nginx")
 
 # Ajustar o Firewall
+print("Ajustando o Firewall...")
 os.system("sudo ufw allow 'Nginx HTTP'")
 
 
 # Instalar Docker
+print("Instalando Docker...")
 os.system("sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D")
 os.system("sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'")
 os.system("sudo apt-get update")
 os.system("sudo apt-get install -y docker-engine")
 
 # Baixando a imagem
+print("Baixando Imagem...")
 os.system("sudo docker pull ubuntu")
 
 # Criando Container app1
+print("Criando Container app1...")
 os.system("sudo docker run -ti --name app1 --hostname app1 ubuntu /bin/bash")
 os.system("sudo docker exec -d app1 apt-get update")
 os.system("sudo docker exec -d app1 apt-get install -y apache2")
@@ -27,6 +31,7 @@ os.system("sudo docker exec -d app1 exit")
 os.system("sudo docker exec -d app1 sudo docker start app1")
 
 # Criando Container app2
+print("Criando Container app1...")
 os.system("sudo docker run -ti --name app2 --hostname app2 ubuntu /bin/bash")
 os.system("sudo docker exec -d app2 apt-get update")
 os.system("sudo docker exec -d app2 apt-get install -y apache2")
@@ -36,6 +41,7 @@ os.system("sudo docker exec -d app2 exit")
 os.system("sudo docker start app2")
 
 # Criando Container app3
+print("Criando Container app1...")
 os.system("sudo docker run -ti --name app3 --hostname app3 ubuntu /bin/bash")
 os.system("sudo docker exec -d app3 apt-get update")
 os.system("sudo docker exec -d app3 apt-get install -y apache2")
@@ -45,6 +51,7 @@ os.system("sudo docker exec -d app3 exit")
 os.system("sudo docker start app3")
 
 # Configurando /etc/hosts
+print("Configurando /etc/hosts...")
 # 172.17.0.0/17 Ordem de IPs de máquinas iniciadas no Docker
 os.system("echo '172.17.0.2	app1.dexter.com.br' >> /etc/hosts")
 os.system("echo '172.17.0.3	app2.dexter.com.br' >> /etc/hosts")
@@ -52,6 +59,7 @@ os.system("echo '172.17.0.4	app3.dexter.com.br' >> /etc/hosts")
 
 
 # Configurando proxy_pass Nginx
+print("Configurando proxy_pass Nginx")
 os.system("echo 'server {listen 80;listen [::]:80;' >> /etc/nginx/conf.d/apps.conf")
 os.system("echo 'location /app1 {proxy_pass http://app1.dexter.com.br:80/;}' >> /etc/nginx/conf.d/apps.conf")
 os.system("echo 'location /app2 {proxy_pass http://app2.dexter.com.br:80/;}' >> /etc/nginx/conf.d/apps.conf")
@@ -59,7 +67,9 @@ os.system("echo 'location /app3 {proxy_pass http://app3.dexter.com.br:80/;}' >> 
 os.system("echo '}' >> /etc/nginx/conf.d/apps.conf")
 
 # Desativa pagina Welcome to nginx
+print("Desativando Welcome")
 os.system("mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.disabled")
 
 # Recarrega o nginx
+print("Recarregando Nginx")
 os.system("nginx -s reload")
