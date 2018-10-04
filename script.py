b66475c28a18fd999b56cb9b0c3809df79cd1ee4
edd7 <edd7@bitbucket.org>
2018-10-04 02:19:47 -0400

    sudo

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import os
 
+#Instalando Nginx
 print("Instalando Nginx...")
 os.system("sudo apt-get update")
 os.system("sudo apt-get install -y nginx")
@@ -22,7 +23,7 @@ os.system("sudo docker pull ubuntu")
 
 # Criando Container app1
 print("Criando Container app1...")
-os.system("sudo docker run -ti --name app1 --hostname app1 ubuntu /bin/bash")
+os.system("sudo docker run -d -ti --name app1 --hostname app1 ubuntu /bin/bash")
 os.system("sudo docker exec -d app1 apt-get update")
 os.system("sudo docker exec -d app1 apt-get install -y apache2")
 os.system("sudo docker exec -d app1 rm /var/www/html/index.html")
@@ -31,8 +32,8 @@ os.system("sudo docker exec -d app1 exit")
 os.system("sudo docker exec -d app1 sudo docker start app1")
 
 # Criando Container app2
-print("Criando Container app1...")
-os.system("sudo docker run -ti --name app2 --hostname app2 ubuntu /bin/bash")
+print("Criando Container app2...")
+os.system("sudo docker run -d -ti --name app2 --hostname app2 ubuntu /bin/bash")
 os.system("sudo docker exec -d app2 apt-get update")
 os.system("sudo docker exec -d app2 apt-get install -y apache2")
 os.system("sudo docker exec -d app2 rm /var/www/html/index.html")
@@ -41,8 +42,8 @@ os.system("sudo docker exec -d app2 exit")
 os.system("sudo docker start app2")
 
 # Criando Container app3
-print("Criando Container app1...")
-os.system("sudo docker run -ti --name app3 --hostname app3 ubuntu /bin/bash")
+print("Criando Container app3...")
+os.system("sudo docker run -d -ti --name app3 --hostname app3 ubuntu /bin/bash")
 os.system("sudo docker exec -d app3 apt-get update")
 os.system("sudo docker exec -d app3 apt-get install -y apache2")
 os.system("sudo docker exec -d app3 rm /var/www/html/index.html")
@@ -53,23 +54,23 @@ os.system("sudo docker start app3")
 # Configurando /etc/hosts
 print("Configurando /etc/hosts...")
 # 172.17.0.0/17 Ordem de IPs de máquinas iniciadas no Docker
-os.system("echo '172.17.0.2	app1.dexter.com.br' >> /etc/hosts")
-os.system("echo '172.17.0.3	app2.dexter.com.br' >> /etc/hosts")
-os.system("echo '172.17.0.4	app3.dexter.com.br' >> /etc/hosts")
+os.system("sudo echo '172.17.0.2	app1.dexter.com.br' >> /etc/hosts")
+os.system("sudo echo '172.17.0.3	app2.dexter.com.br' >> /etc/hosts")
+os.system("sudo echo '172.17.0.4	app3.dexter.com.br' >> /etc/hosts")
 
 
 # Configurando proxy_pass Nginx
-print("Configurando proxy_pass Nginx")
-os.system("echo 'server {listen 80;listen [::]:80;' >> /etc/nginx/conf.d/apps.conf")
-os.system("echo 'location /app1 {proxy_pass http://app1.dexter.com.br:80/;}' >> /etc/nginx/conf.d/apps.conf")
-os.system("echo 'location /app2 {proxy_pass http://app2.dexter.com.br:80/;}' >> /etc/nginx/conf.d/apps.conf")
-os.system("echo 'location /app3 {proxy_pass http://app3.dexter.com.br:80/;}' >> /etc/nginx/conf.d/apps.conf")
-os.system("echo '}' >> /etc/nginx/conf.d/apps.conf")
+print("Configurando proxy_pass Nginx...")
+os.system("sudo echo 'server {listen 80;listen [::]:80;' >> /etc/nginx/conf.d/apps.conf")
+os.system("sudo echo 'location /app1 {proxy_pass http://app1.dexter.com.br:80/;}' >> /etc/nginx/conf.d/apps.conf")
+os.system("sudo echo 'location /app2 {proxy_pass http://app2.dexter.com.br:80/;}' >> /etc/nginx/conf.d/apps.conf")
+os.system("sudo echo 'location /app3 {proxy_pass http://app3.dexter.com.br:80/;}' >> /etc/nginx/conf.d/apps.conf")
+os.system("sudo echo '}' >> /etc/nginx/conf.d/apps.conf")
 
 # Desativa pagina Welcome to nginx
 print("Desativando Welcome")
-os.system("mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.disabled")
+os.system("sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.disabled")
 
 # Recarrega o nginx
 print("Recarregando Nginx")
-os.system("nginx -s reload")
+os.system("sudo nginx -s reload")
